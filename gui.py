cae97cfae0add0a879bd5f652047e87ae86a7ed0
wfsbear <47589206+ilikecinepol@users.noreply.github.com>
2023-08-31 09:51:50 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,20 +2,25 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.animation import Animation
+from kivy.uix.popup import Popup
+import serial
 
 # Устанавливаем размер окна
 Window.size = (1024, 600)
-
+reverse = False
 # Загружаем стилизацию через файл kv
 Builder.load_string('''
 <CarControlPanel>:
     orientation: 'vertical'
     spacing: '5dp'
     padding: '10dp'
+    on_parent:
+        self.z = float('inf') if self.parent else 0
     canvas.before:
+    
         Rectangle:
             pos: self.pos
             size: self.size
@@ -30,26 +35,34 @@ Builder.load_string('''
     #     keep_ratio: False  # Отключаем сохранение пропорций фона
     #     pos_hint: {'center_x': 0.5, 'center_y': 0.5}  # Позиционируем по центру
     RelativeLayout:          
-        Image:
-            id: speedometr
-            source: 'pict/speedometr.png'  # Изображение по умолчанию
-            allow_stretch: True
-            keep_ratio: True
+        Button:
+            text: 'Настройки'
+            # background_normal: 'settings_icon.png'
+            # background_down: 'settings_icon.png'
             size_hint: None, None
-            size: '300dp', '300dp'
-            pos_hint: {'center_x': 0.2, 'center_y': 0.5}
-            z: 1  # Устанавливаем порядок отображения нижней картинки
-    
-        Image:
-            id: arrow
-            source: 'pict/arrow_angle.png'  # Путь к верхней картинке
-            allow_stretch: True
-            keep_ratio: True
-            size_hint: None, None
-            size: '300dp', '300dp'
-            pos_hint: {'center_x': 0.2, 'center_y': 0.5}  # Задание координат положения
-            angle: 0  # Начальный угол поворота
-            z: 2 # Устанавливаем порядок отображения верхней картинки
+            size: 150, 50
+            pos_hint: {'right': 1, 'top': 1}
+            on_release: root.show_settings_popup()
+        # Image:
+        #     id: speedometr
+        #     source: 'pict/speedometr.png'  # Изображение по умолчанию
+        #     allow_stretch: True
+        #     keep_ratio: True
+        #     size_hint: None, None
+        #     size: '300dp', '300dp'
+        #     pos_hint: {'center_x': 0.2, 'center_y': 0.5}
+        #     z: 1  # Устанавливаем порядок отображения нижней картинки
+        # 
+        # Image:
+        #     id: arrow
+        #     source: 'pict/arrow_angle.png'  # Путь к верхней картинке
+        #     allow_stretch: True
+        #     keep_ratio: True
+        #     size_hint: None, None
+        #     size: '300dp', '300dp'
+        #     pos_hint: {'center_x': 0.2, 'center_y': 0.5}  # Задание координат положения
+        #     angle: 0  # Начальный угол поворота
+        #     z: 2 # Устанавливаем порядок отображения верхней картинки
     
     Button:
         text: 'Включение фар'
@@ -125,9 +138,34 @@ Builder.load_string('''
         text: 'Дистанционное управление'
         size_hint_y: None
         height: '48dp'
-        on_release: root.rotate_car_image()
+        on_release: root.rc_control()
         background_color: 0, 0, 1, 1  # Синий цвет (R, G, B, A)
-        on_press: root.toggle_button_color(self); root.rotate_car_image()  # Изменение цвета при нажатии      
+        on_press: root.toggle_button_color(self)  # Изменение цвета при нажатии
+    
+<SettingsPopup>:
+    title: 'Настройки'
+    size_hint: None, None
+    size: 300, 200
+
+    BoxLayout:
+        orientation: 'vertical'
+        padding: 20
+
+        # Label:
+        #     text: 'Здесь могут быть ваши настройки'
+        Button:
+            text: 'Инверсия руля'
+            size_hint_y: None
+            height: '48dp'
+            on_release: root.reverse_control()
+            # background_color: 0, 0, 1, 1  # Синий цвет (R, G, B, A)
+            on_press: root.toggle_button_color(self)  # Изменение цвета при нажатии
+        Button:
+            text: 'Закрыть'
+            size_hint: None, None
+            size: 100, 40
+            pos_hint: {'center_x': 0.5}
+            on_release: root.dismiss()
 ''')
 
 
@@ -137,6 +175,10 @@ class CarControlPanel(BoxLayout):
         car_image = self.ids.arrow
         anim = Animation(angle=angle_degrees, duration=2)
         anim.start(car_image)
+
+    def show_settings_popup(self):
+        settings_popup = SettingsPopup()
+        settings_popup.open()
 
     def reset_button_colors(self, buttons_to_reset):
         for button in buttons_to_reset:
@@ -179,10 +221,27 @@ class CarControlPanel(BoxLayout):
         print('rc_control')
 
 
+class SettingsPopup(Popup):
+    def reverse_control(self):
+        global reverse
+        print('reverse= ', reverse)
+        reverse = not reverse
+
+    def toggle_button_color(self, button):
+        if button.background_color == [0.8, 0.8, 0.8, 1]:
+            button.background_color = [0.5, 0.5, 0.5, 1]  # Серый цвет
+        else:
+            button.background_color = [0.8, 0.8, 0.8, 1]
+
+
 class CarControlApp(App):
     def build(self):
         return CarControlPanel()
 
 
 if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser.write(b"Hello from Raspberry Pi!\n")
+    ser.flush()
     CarControlApp().run()
+
